9e1850d5bea89bc227658d3ae02ae4fac660e5fd
Paul MASSON <pmasson@e1r11p21.42.fr>
2019-08-12 15:46:11 +0200

    modif ft_msg_int verif null string

--- libft/srcs/ft_msg_int.c
+++ libft/srcs/ft_msg_int.c
@@ -6,7 +6,7 @@
 /*   By: pmasson <pmasson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/25 13:56:44 by pmasson           #+#    #+#             */
-/*   Updated: 2019/02/25 13:58:12 by pmasson          ###   ########.fr       */
+/*   Updated: 2019/08/12 15:45:30 by pmasson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int	ft_msg_int(int fd, char *msg, int ret)
 {
-	write(fd, msg, ft_strlen(msg));
+	if (msg != NULL)
+		write(fd, msg, ft_strlen(msg));
 	return (ret);
 }
